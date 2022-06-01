10b7c24fce920a339b2d37c2498bc65ba072f046
Kateryna Tekmenzhi <kateryna.koltunova@gmail.com>
2022-06-01 13:37:02 -0700

    creating constructor to declare classesin seperate employee.js file

--- src/generate.js
+++ src/generate.js
@@ -1,4 +1,4 @@
-// generates the code for the given schema
+// generates the html for the page
 function generateHTML(newEmployee) {
     return `
     <!DOCTYPE html>
@@ -15,6 +15,13 @@ function generateHTML(newEmployee) {
         <header>
             <h1 class="text-center">Employee Directory</h1>
         </header>
+        <section class="card-group">
+            ${newEmployee.map(generateCards)}
+        </section>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     </body>
     </html>`    
 };
+
+// function generateCards()
