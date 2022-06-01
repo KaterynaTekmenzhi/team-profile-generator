b7ae6c6f01b352f6800c5a9a630d8b2c1c477d99
Kateryna Tekmenzhi <kateryna.koltunova@gmail.com>
2022-06-01 14:35:01 -0700

    creating manager constructor

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 // packages needed for this application
 const inquirer = require('inquirer');
 
+// importing 
+const Manager = require('./lib/Manager');
+
 // array for adding generated employees to
 const newEmployee = [];
 
-const createLog = () => {
+const createManager = () => {
     return inquirer.prompt([
         {
             type: 'input',
@@ -38,11 +41,51 @@ const createLog = () => {
         },
     ])
     .then((answer) => {
-        if (!answer.id) {
-            console.log('Please enter a valid id');
-            getIdNumber();
-        } else {
-            console.log(answer);
-        }
+        const manager = new Manager(answer.name, answer.id, answer.email, answer.officeNumber);
+        newEmployee.push(manager);
+        console.log(newEmployee);
     })
 }
+
+const createEngineer = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the engineer name?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the engineer email?',
+            validate: function(value) {
+                var pass = value.match(
+                    /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/
+                );
+                if (pass) {
+                    return true;
+                }
+                console.log('Please enter a valid email address');
+                return false;
+            }
+        },
+        {
+            name: 'id',
+            message: 'What is the employee id?',
+            type: 'number'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is the engineer github username?',
+        },
+    ])
+    .then((answer) => {
+        const engineer = new Engineer(answer.name, answer.id, answer.email, answer.github);
+        newEmployee.push(engineer);
+        console.log(newEmployee);
+    })
+}
+
+createManager();
+console.log(newEmployee);
